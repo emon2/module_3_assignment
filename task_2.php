186b89b7646dbed052c19f1e982cfb304c499b22
emon2 <43115944+emon2@users.noreply.github.com>
2023-09-29 11:19:23 +0600

    Update task_2.php

--- task_2.php
+++ task_2.php
@@ -11,13 +11,8 @@ function remove_even($array) {
   return array_filter($array, "is_odd");
 }
 
-function remove_even($array) {
-  // Define the callback function
-  function is_odd($num) {
-    // Check if the number is odd by using the modulo operator
-    return $num % 2 != 0;
-  }
-  // Filter the array using the callback function
-  return array_filter($array, "is_odd");
-}
+// Call the function with the $numbers array
+$result = remove_even($numbers);
+// Print the resulting array
+print_r($result);
 ?>
